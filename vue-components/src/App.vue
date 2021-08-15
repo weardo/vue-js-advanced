58dbc04cc04e7d30ee8b5233c461df57cc398860
abhishekyocket <abhishek.janjalkar@yocket.in>
2021-08-15 17:59:49 +0530

    Portal Components

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,35 +1,21 @@
 <template>
 <div>
-  <button @click="activeTab = 'TabA'">Tab A</button>
-  <button @click="activeTab = 'TabB'">Tab B</button>
-  <button @click="activeTab = 'TabC'">Tab C</button>
-
-  <keep-alive>
-  <component :is="activeTab" />
-  </keep-alive>
-
-  <!-- <TabA v-if="activeTab === 'TabA'" />
-  <TabB v-if="activeTab === 'TabB'" />
-  <TabC v-if="activeTab === 'TabC'" /> -->
-
+  <teleport to='#portal-root'>
+  <Portal />
+  </teleport>
 </div>
 </template>
 
 <script>
-import TabA from './components/TabA.vue'
-import TabB from './components/TabB.vue'
-import TabC from './components/TabC.vue'
+import Portal from './components/Portal.vue'
 
 export default {
   name: 'App',
   components: {
-    TabA,
-    TabB, 
-    TabC
+    Portal,
   },
   data() {
     return { 
-      activeTab: 'TabA' 
     }
   },
   methods: {
